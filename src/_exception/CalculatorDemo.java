19925fcac3fc8b5027995f21e3c8cf19a2aa905e
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-08 20:34:47 +0900

    Update 예외 뒷수습

--- src/_exception/CalculatorDemo.java
+++ src/_exception/CalculatorDemo.java
@@ -20,7 +20,38 @@ class Calculator{
             System.out.print(this.left/this.right);
             System.out.print(" 입니다.");
         } catch(Exception e){
-            System.out.println("오류가 발생했습니다 : "+e.getMessage());
+            System.out.println("\n\ne.getMessage()\n : "+e.getMessage());
+            System.out.println("\n\ne.toString()\n" + e.toString());
+            System.out.println("\n\ne.printStackTrace()");
+            e.printStackTrace();
+            /*
+            e.getMessage()
+            - 오류에 대한 기본적인 내용을 출력해준다. 상세하지 않다.
+
+            e.toString()
+            - e.getMessage()보다 더 자세한 예외 정보를 제공한다.
+
+            e.printStackTrace()
+            -  printStackTrace는 리턴값이 없다. 이 메소드를 호출하면 메소드가 내부적으로 예외 결과를 화면에 출력한다.
+               printStackTrace는 가장 자세한 예외 정보를 제공한다.
+
+            [출력 결과]
+            계산결과는
+
+            e.getMessage()
+            / by zero
+
+
+            e.toString()
+            java.lang.ArithmeticException: / by zero
+
+
+            e.printStackTrace()
+            java.lang.ArithmeticException: / by zero
+                at org.opentutorials.javatutorials.exception.Calculator.divide(CalculatorDemo.java:11)
+                at org.opentutorials.javatutorials.exception.CalculatorDemo.main(CalculatorDemo.java:25)
+
+             */
         }
     }
 }
