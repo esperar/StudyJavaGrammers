bc3576ab16d2984bf6bc02cb3af66500d4862eeb
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-09 11:48:31 +0900

    finally

--- src/_exception/exceptionDemo.java
+++ src/_exception/exceptionDemo.java
@@ -8,7 +8,7 @@ class A{
         arr[2] = 20;
     }
     /*
-    다중 catch문을 사용한다 에러를 좀더 세부적으로 검출해 다양한 상황에서 쉽게 대처할 수 있다.
+    다중 catch 문을 사용한다 에러를 좀더 세부적으로 검출해 다양한 상황에서 쉽게 대처할 수 있다.
      */
     public void z(int first , int second){
         try {
@@ -19,6 +19,12 @@ class A{
             System.out.println("ArithmeticException");
         } catch(Exception e){
             System.out.println("Exception");
+        } finally {
+            /*
+            finally 는 try 부분의 코드 실행이 예외가 발생했건 안했건 무조건 실행되는 부분이다.
+            사용 : 데이터베이스에 대한 접속을 끊어주기위해 사용한다.(누적 제거)
+             */
+            System.out.println("finally");
         }
     }
 }
@@ -27,8 +33,8 @@ class A{
 public class exceptionDemo {
     public static void main(String[] args) {
         A a = new A();
-        a.z(10, 0); // ArrayIndexOutofBoundsException
-        a.z(1,0); // by zero ArithmeticException
-        a.z(2,1); // 2
+        a.z(10, 0); // ArrayIndexOutofBoundsException finally
+        a.z(1,0); // by zero ArithmeticException finally
+        a.z(2,1); // 2 finally
     }
 }
